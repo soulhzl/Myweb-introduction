7df707c327bd9fdf63cd5860e8bb603a3d4c6f78
soulhzl <h574114947zl@163.com>
2018-05-06 19:36:30 +0800

    delete

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,6 +1,5 @@
 $(function(){
 	$("#fullPageId").fullpage({
-				// sectionsColor : ['red', 'yellow', 'blue'],
 				afterLoad : function(anchor, index){
 					if (index === 2) {
 						var now = index - 1;
