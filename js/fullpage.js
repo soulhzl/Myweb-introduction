5cca8f6ce44305f31b6a175027514e42499a1cca
soulhzl <h574114947zl@163.com>
2018-05-14 20:19:12 +0800

    update

--- js/fullpage.js
+++ js/fullpage.js
@@ -1,10 +1,13 @@
 $(function(){
 	$("#fullPageId").fullpage({
-				afterLoad : function(anchor, index){
-					if (index === 2) {
-						var now = index - 1;
-						$(".section").eq(now).addClass("current");
-					}
-				}
-			});
+		anchors : ['page1', 'page2', 'page3', 'page4'],
+		menu : "#menu", 
+		afterLoad : function(anchor, index){
+			var now = index - 1;
+			$('.section').eq(now).addClass('sectionani');
+			if (index === 4) {
+					$('#gjlink').css('display', 'block');
+			}
+		}
+	});
 });
